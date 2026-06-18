11e78772c169362d1ed3dd60bca31afef90e89fb
Helida <helidaxerfan@gmail.com>
2026-06-18 14:14:15 -0400

    bolaofinal

--- Bolao/bolaov2.cpp
+++ Bolao/bolaov2.cpp
@@ -1,17 +1,25 @@
-/*Hélida DIas Batista Xerfan
-Leonardo Tupinambá
-Lorraine Isabele
-*/
-
 #include <iostream>
 #include <string>
-#include <iomanip>
-#include <clocale>
+#include <vector>
+#include <algorithm>
 #ifdef _WIN32
 #include <windows.h>
 #endif
 
 using namespace std;
+
+// 1. Criação de estruturas para modelar as entidades do sistema
+struct Partida {
+    string time1;
+    string time2;
+    int resultado1 = -1; // -1 indica que a partida ainda não ocorreu
+    int resultado2 = -1;
+};
+
+struct Apostador {
+    string nome;
+    int pontos = 0;
+};
 
 int calcularPontos(int p1, int p2, int resultado1, int resultado2) {
     if (p1 == resultado1 && p2 == resultado2)
@@ -25,86 +33,77 @@ int calcularPontos(int p1, int p2, int resultado1, int resultado2) {
         return 0;
 }
 
-void cadastroApostador() {
-    string nome;
+// Passando o vetor por referência para salvar o dado permanentemente
+void cadastroApostador(vector<Apostador>& apostadores) {
+    Apostador novoApostador;
     cout << "Digite o nome do apostador: ";
-    cin >> nome;
-    cout << "Apostador \"" << nome << "\" cadastrado com sucesso!" << endl;
+    cin >> novoApostador.nome;
+    apostadores.push_back(novoApostador);
+    cout << "Apostador \"" << novoApostador.nome << "\" cadastrado com sucesso!\n";
 }
 
-void adicionarPartida() {
-    string partida;
-    cout << "Digite o nome da partida a ser adicionada: ";
-    cin >> partida;
-    cout << "Partida \"" << partida << "\" adicionada com sucesso!" << endl;
+// Agora adicionamos partidas dinamicamente
+void adicionarPartida(vector<Partida>& partidas) {
+    Partida novaPartida;
+    cout << "Digite o nome do primeiro time (Mandante): ";
+    cin >> novaPartida.time1;
+    cout << "Digite o nome do segundo time (Visitante): ";
+    cin >> novaPartida.time2;
+    
+    // Simulação: definindo o resultado real da partida no momento do cadastro 
+    // (Em um sistema real, isso seria feito em outro momento)
+    cout << "Qual foi o placar real do " << novaPartida.time1 << "? ";
+    cin >> novaPartida.resultado1;
+    cout << "Qual foi o placar real do " << novaPartida.time2 << "? ";
+    cin >> novaPartida.resultado2;
+
+    partidas.push_back(novaPartida);
+    cout << "Partida \"" << novaPartida.time1 << " x " << novaPartida.time2 << "\" adicionada!\n";
 }
 
-void realizarApostas() {
-    string Apostador;
-    string partida;
-    string aposta1;
-    string aposta2;
-    string aposta3;
-    
-    int pontos1 = 0;
-    int pontos2 = 0;
-    int pontos3 = 0;
+void realizarApostas(vector<Apostador>& apostadores, const vector<Partida>& partidas) {
+    if (apostadores.empty() || partidas.empty()) {
+        cout << "Erro: É necessário ter pelo menos um apostador e uma partida cadastrados.\n";
+        return;
+    }
 
-    cout << "1º Apostador:" << endl;
-    cout << "Digite seu nome: ";
-    cin >> Apostador;
-    
-    // Resultados 
-    int brasil = 3, egito = 0;
-    int russia = 1, marrocos = 1;
-    int franca = 1, argentina = 2;
-    int alemanha = 4, costaRica = 2;
-    
     int p1, p2;
-    
-    // Brasil x Egito
-    cout << "Apostas:" << Apostador << endl;
-    cout << "Brasil x Egito" << endl;
-    cout<< "Brasil: " << endl;
-    cin >> p1;
-    cout<< "Egito: "<< endl;
-    cin >> p2;
-    
-    pontos1 += calcularPontos(p1, p2, brasil, egito);
-    
-    
-    // Russia x Marrocos
-    cout << "Russia x Marrocos"<< endl;
-    cout<< "Russia: " << endl;
-    cin >> p1;
-    cout<< "Marrocos: "<< endl;
-    cin >> p2;
-    
-    pontos1 += calcularPontos(p1, p2, russia, marrocos);
-    
-    
-    // França x Argentina
-    cout << "Franca x Argentina"<< endl;
-    cout<< "Franca: " << endl;
-    cin >> p1;
-    cout<< "Argentina: "<< endl;
-    cin >> p2;
-    
-    pontos1 += calcularPontos(p1, p2, franca, argentina);
-    
-     
-    // Alemanha x Costa Rica
-    cout << "Alemanha x Costa Rica"<< endl;
-    cout<< "Alemanha: " << endl;
-    cin >> p1;
-    cout<< "Costa Rica: "<< endl;
-    cin >> p2;
-    
-    pontos1 += calcularPontos(p1, p2, alemanha, costaRica);
 
-    cin >> aposta2;
-    cout << "Aposta de \"" << aposta2 << "\" na partida \"" << partida
-    << "\" registrada com sucesso!" << endl;
+    for (size_t i = 0; i < apostadores.size(); ++i) {
+        cout << "\nApostas de: " << apostadores[i].nome << endl;
+        
+        // Zera os pontos para recalcular caso refaçam as apostas
+        apostadores[i].pontos = 0; 
+
+        for (size_t j = 0; j < partidas.size(); ++j) {
+            cout << partidas[j].time1 << " x " << partidas[j].time2 << endl;
+            cout << partidas[j].time1 << ": ";
+            cin >> p1;
+            cout << partidas[j].time2 << ": ";
+            cin >> p2;
+            
+            apostadores[i].pontos += calcularPontos(p1, p2, partidas[j].resultado1, partidas[j].resultado2);
+        }
+        cout << "***************************" << endl;
+    }
+}
+
+void visualizarRanking(vector<Apostador> apostadores) {
+    if (apostadores.empty()) {
+        cout << "Nenhum apostador cadastrado." << endl;
+        return;
+    }
+
+    // Ordenação utilizando uma função lambda moderna
+    sort(apostadores.begin(), apostadores.end(), [](const Apostador& a, const Apostador& b) {
+        return a.pontos > b.pontos;
+    });
+
+    cout << "=== Ranking de Pontuação ===" << endl;
+    for (size_t i = 0; i < apostadores.size(); ++i) {
+        cout << (i + 1) << "º Lugar: " << apostadores[i].nome << " - " << apostadores[i].pontos << " pontos" << endl;
+    }
+    cout << "***************************" << endl;
 }
 
 int main() {
@@ -115,197 +114,39 @@ int main() {
     #endif
 
     int opcao;
-    string entrada;
+    vector<Apostador> apostadores;
+    vector<Partida> partidas;
 
     do {
-        cout << "=== Bem-vindo ao Sistema de Bolão ===" << endl;
-        cout << endl;
+        cout << "\n=== Bem-vindo ao Sistema de Bolão ===" << endl;
         cout << "1. Cadastrar Apostador" << endl;
         cout << "2. Adicionar Partida" << endl;
         cout << "3. Realizar Apostas" << endl;
-        cout << "4. Mostrar Ranking" <<endl;
+        cout << "4. Visualizar Ranking" << endl;
         cout << "5. Sair" << endl;
         cout << "Escolha uma opção: ";
         cin >> opcao;
-        cin.ignore(); // limpar buffer
 
         switch (opcao) {
-            case 1: {
-                cadastroApostador();
+            case 1:
+                cadastroApostador(apostadores);
                 break;
-            }
-            case 2: {
-                adicionarPartida();
+            case 2:
+                adicionarPartida(partidas);
                 break;
-            }
-            case 3: {
-              realizarApostas();
+            case 3:
+                realizarApostas(apostadores, partidas);
                 break;
-            }
-            case 4: {
-                cout << "Obrigado por participar do Bolão!" << "! Até logo!" << endl;
+            case 4:
+                visualizarRanking(apostadores);
                 break;
-            }
+            case 5:
+                cout << "Obrigado por participar do Bolão! Até logo!" << endl;
+                break;
             default:
                 cout << "Opção inválida. Por favor, tente novamente." << endl;
         }
-        cout << endl; // Adiciona uma linha em branco para melhor legibilidade
-    } while (opcao != 4);
-
-        string apostador1, apostador2, apostador3;
-
-        int pontos1 = 0, pontos2 = 0, pontos3 = 0;
-
-    // Resultados 
-    
-/*
-    
- //   cout << "***************************" << endl;
-  //  cout <<endl;
-  //  cout << "2º Apostador:" << endl;
-  //  cout << "Digite seu nome: ";
-  //  cin >> apostador2;
-
-    
-  //  cout << "Apostas:" << apostador2 << endl;
-  //  cout << "Brasil x Egito" << endl;
-  //  cout<< "Brasil: " << endl;
-  //  cin >> p1;
-  //  cout<< "Egito: "<< endl;
-   // cin >> p2;
-
-
- //   pontos2 += calcularPontos(p1, p2, brasil, egito);
-
-
-
-    // Russia x Marrocos
- //   cout << "Russia x Marrocos"<< endl;
- //   cout<< "Russia: " << endl;
- //   cin >> p1;
-  //  cout<< "Marrocos: "<< endl;
-  //  cin >> p2;
-
-    // pontos2 += calcularPontos(p1, p2, russia, marrocos);
-
-    
-    // // França x Argentina
-    // cout << "Franca x Argentina"<< endl;
-    // cout<< "Franca: " << endl;
-    // cin >> p1;
-    // cout<< "Argentina: "<< endl;
-    // cin >> p2;
-
-    // pontos2 += calcularPontos(p1, p2, franca, argentina);
-
-
-
-
-    // // Alemanha x Costa Rica
-    // cout << "Alemanha x Costa Rica"<< endl;
-    // cout<< "Alemanha: " << endl;
-    // cin >> p1;
-    // cout<< "Costa Rica: "<< endl;
-    // cin >> p2;
-
-    // pontos2 += calcularPontos(p1, p2, alemanha, costaRica);
-
-
-
-
-
-
-    // // ***********************************************************
-    // cout << "***************************" << endl;
-    // cout <<endl;
-    // cout << "3º Apostador:" << endl;
-    // cout << "Digite seu nome: ";
-    // cin >> apostador3;
-
-    
-    // cout << "Apostas:" << apostador3 << endl;
-    // cout << "Brasil x Egito" << endl;
-    // cout<< "Brasil: " << endl;
-    // cin >> p1;
-    // cout<< "Egito: "<< endl;
-    // cin >> p2;
-
-
-    // pontos3 += calcularPontos(p1, p2, brasil, egito);
-
-
-
-    // // Russia x Marrocos
-    // cout << "Russia x Marrocos"<< endl;
-    // cout<< "Russia: " << endl;
-    // cin >> p1;
-    // cout<< "Marrocos: "<< endl;
-    // cin >> p2;
-
-    // pontos3 += calcularPontos(p1, p2, russia, marrocos);
-
-    
-    // // França x Argentina
-    // cout << "Franca x Argentina"<< endl;
-    // cout<< "Franca: " << endl;
-    // cin >> p1;
-    // cout<< "Argentina: "<< endl;
-    // cin >> p2;
-
-    // pontos3 += calcularPontos(p1, p2, franca, argentina);
-
-
-
-
-    // // Alemanha x Costa Rica
-    // cout << "Alemanha x Costa Rica"<< endl;
-    // cout<< "Alemanha: " << endl;
-    // cin >> p1;
-    // cout<< "Costa Rica: "<< endl;
-    // cin >> p2;
-
-    // pontos3 += calcularPontos(p1, p2, alemanha, costaRica);
-
-*/
-
-    cout << "***************************" << endl;
-    cout << "********RESULTADO********";
-    cout << "***************************" << endl;
-    cout <<endl;
-
-    if (pontos1 >= pontos2 && pontos1 >= pontos3) {
-        cout << "\n1º Lugar: " << apostador1 << ": " << pontos1 << " pontos\n";
-
-        if (pontos2 >= pontos3) {
-            cout << "2º Lugar: " << apostador2 << ": " << pontos2 << " pontos\n";
-            cout << "3º Lugar: " << apostador3 << ": " << pontos3 << " pontos\n";
-        } else {
-            cout << "2º Lugar: " << apostador3 << ": " << pontos3 << " pontos\n";
-            cout << "3º Lugar: " << apostador2 << ": " << pontos2 << " pontos\n";
-        }
-    }
-    else if (pontos2 >= pontos1 && pontos2 >= pontos3) {
-        cout << "\n1º Lugar: " << apostador2 << ": " << pontos2 << " pontos\n";
-
-        if (pontos1 >= pontos3) {
-            cout << "2º Lugar: " << apostador1 << ": " << pontos1 << " pontos\n";
-            cout << "3º Lugar: " << apostador3 << ": " << pontos3 << " pontos\n";
-        } else {
-            cout << "2º Lugar: " << apostador3 << ": " << pontos3 << " pontos\n";
-            cout << "3º Lugar: " << apostador1 << ": " << pontos1 << " pontos\n";
-        }
-    }
-    else {
-        cout << "\n1º Lugar: " << apostador3 << ": " << pontos3 << " pontos\n";
-
-        if (pontos1 >= pontos2) {
-            cout << "2º Lugar: " << apostador1 << ": " << pontos1 << " pontos\n";
-            cout << "3º Lugar: " << apostador2 << ": " << pontos2 << " pontos\n";
-        } else {
-            cout << "2º Lugar: " << apostador2 << ": " << pontos2 << " pontos\n";
-            cout << "3º Lugar: " << apostador1 << ": " << pontos1 << " pontos\n";
-        }
-    }
+    } while (opcao != 5);
 
     return 0;
 }
